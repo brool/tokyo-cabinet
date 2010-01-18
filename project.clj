2ca506c5838a83428c885f9f40a759c4a9144f4b
Isaac Praveen Abraham <icylisper@gmail.com>
2010-01-18 14:13:08 +0530

    added layout as required by lein

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tokyo-cabinet "0.2.1"
+  :description "tokyo cabinet client in clojure"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [rabbitmq-client "1.7.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
